57a815868d201314fa70bd22c5ec5da20c38035c
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-06-05 23:44:27 -0700

    Adding website

---
+++ website/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <h1 id="title">Karthik Dharmarajan</h1>
+      <div id="title-feature"></div>
+      <div id="intro-section">
+      </div>
+      <div id="project-section">
+          <h2 id="projects-title">Projects</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
