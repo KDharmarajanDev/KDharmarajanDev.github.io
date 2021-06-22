edc0c714241f2c51bc72d1630096fbb79ff32a2a
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-06-21 23:53:33 -0700

    Fixed centering of buttons

--- website/src/ContactSegment.js
+++ website/src/ContactSegment.js
@@ -1,15 +1,13 @@
-import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import ReceiptIcon from '@material-ui/icons/Receipt';
 
 const useStyles = makeStyles({
     button: {
-        // marginLeft: 'auto',
-        // marginRight: 'auto',
+        margin: 'auto',
         borderColor: 'white',
         color: 'white'
     },
@@ -33,28 +31,22 @@ export default function ContactSegment(props) {
         window.open('https://github.com/KDharmarajanDev/');
     };
 
-    return (<Grid container id="contact-segment-container" justify="space-between" spacing={5} wrap="wrap" direction="row">
+    return (<Grid container id="contact-segment-container" align="center" justify="center" spacing={5} wrap="wrap" direction="row" alignItems="center">
                 <Grid xs={12} sm={6} md={3} item>
-                    <Card key="LinkedInButton" className={classes.root}>
-                        <IconButton className={classes.button} onClick={clickCallbackLinkedIn}>
-                            <LinkedInIcon/>
-                        </IconButton>
-                    </Card >
+                    <Button variant="outlined" className={classes.button} onClick={clickCallbackLinkedIn} startIcon={<LinkedInIcon/>}>
+                        LinkedIn
+                    </Button>
                 </Grid>
                 <Grid xs={6} md={3} item>
-                    <Card key="GitHubButton" className={classes.root}>
-                        <IconButton className={classes.button} onClick={clickCallbackGitHub}>
-                            <GitHubIcon/>
-                        </IconButton>
-                    </Card>
+                    <Button variant="outlined" className={classes.button} onClick={clickCallbackGitHub} startIcon={<GitHubIcon/>}>
+                        GitHub
+                    </Button>
                 </Grid>
                 <Grid xs={6} md={3} item>
-                    <Card key="ResumeButton" className={classes.root}>
-                        <Button variant="outlined" className={classes.button} 
-                            onClick={clickCallbackGitHub}>
-                            Resume
-                        </Button>
-                    </Card>
+                    <Button variant="outlined" className={classes.button} 
+                        onClick={clickCallbackGitHub} startIcon={<ReceiptIcon/>}>
+                        Resume
+                    </Button>
                 </Grid>
             </Grid>);
 }
