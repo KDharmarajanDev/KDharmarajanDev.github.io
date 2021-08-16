5c25c7f9716cc202a39d701fc4158946223003e4
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-08-15 20:06:11 -0700

    Temporarily commented out some of my projects

--- website/src/App.js
+++ website/src/App.js
@@ -16,21 +16,21 @@ const projects = [
     + "Android operating system that allows users to control an Arduino operating RGB LED Strips with over one billion possible sequences.",
     technologies: ["C++", "Java"]
   },
-  {
-    name: "Knot Visualizer",
-    image: "./assets/LEDLightStripScheduler.png",
-    link: "https://github.com/KDharmarajanDev/knot-visualizer-app",
-    description: "Knot Visualizer is an iOS application fueled by ARKit that displays the progression of knot tying in "
-    + "one's environment with the goal of educating the end user.",
-    technologies: ["Swift"]
-  },
-  {
-    name: "Drone Control Web Server",
-    image: "./assets/LEDLightStripScheduler.png",
-    link: "https://github.com/KDharmarajanDev/drone-control-web-server",
-    description: "Drone Control Web Server is a modular full-stack application that allows for ssensor data streaming and data plotting.",
-    technologies: ["React", "Node.JS", "ROS"]
-  },
+  // {
+  //   name: "Knot Visualizer",
+  //   image: "./assets/LEDLightStripScheduler.png",
+  //   link: "https://github.com/KDharmarajanDev/knot-visualizer-app",
+  //   description: "Knot Visualizer is an iOS application fueled by ARKit that displays the progression of knot tying in "
+  //   + "one's environment with the goal of educating the end user.",
+  //   technologies: ["Swift"]
+  // },
+  // {
+  //   name: "Drone Control Web Server",
+  //   image: "./assets/LEDLightStripScheduler.png",
+  //   link: "https://github.com/KDharmarajanDev/drone-control-web-server",
+  //   description: "Drone Control Web Server is a modular full-stack application that allows for ssensor data streaming and data plotting.",
+  //   technologies: ["React", "Node.JS", "ROS"]
+  // },
   {
     name: "CoinConnect",
     image: CoinConnect,
